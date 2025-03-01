e715052300db6af24835541aea795c0f6cceb019
Arhenniuss <ayusmansinghal@gmail.com>
2025-03-01 03:15:50 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/WEBDEV01/', // Set the base to your repo name
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
